543b19589849d503680808137ef0e7879dca1f32
Dawit Andargachew <dawit1111111@gmail.com>
2022-03-22 23:40:20 +0300

    How Many Numbers Are Smaller Than the Current Number is solved

---
+++ How Many Numbers Are Smaller Than the Current Number/smaller_than_current_number.java
@@ -0,0 +1,16 @@
+class Solution {
+    static int[] smallerNumbersThanCurrent(int[] nums) {
+
+        int one[] = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int counter = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] > nums[j])
+                    counter++;
+            }
+            one[i] = counter;
+        }
+        return one;
+
+    }
+}
