34d33665279a9d46c295dad984d05147c5c9ee0a
Dawit Andargachew <dawit1111111@gmail.com>
2022-06-30 11:49:16 +0300

    done

--- Kth Largest Integer in the Array/solution.java
+++ Kth Largest Integer in the Array/solution.java
@@ -3,12 +3,14 @@ import java.util.Arrays;
 
 class Solution {
     public String kthLargestNumber(String[] nums, int k) {
-          BigInteger[] arr = new BigInteger[nums.length];
+
+        BigInteger[] arr = new BigInteger[nums.length];
+
         for (int i = 0; i < nums.length; i++) {
             arr[i] = new BigInteger(nums[i]);
         }
         Arrays.sort(arr);
-        return arr[nums.length - k ].toString();
+        return arr[nums.length - k].toString();
     }
 
 }
