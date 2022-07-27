c4dcff21c234c6283ffd827e9188cc7f3f74925a
Dawit Andargachew <dawit1111111@gmail.com>
2022-07-27 16:16:27 +0300

    theater square is done

---
+++ Theater Square/solution.java
@@ -0,0 +1,16 @@
+class solution {
+
+    public static int theater_square(int m, int n, int a) {
+        int width = Math.round((float) m / a); // number of square needed for width
+        int height = Math.round((float) n / a); // number of squares need for height
+
+        // the total amount of squares need to cover the field is widht * height
+        return width * height;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("me is " + theater_square(300, 30, 4));
+
+    }
+
+}
