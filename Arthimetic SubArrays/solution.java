a653b24a76059dc99bfb2661a6f3efdfc38a5299
Dawit Andargachew <hereisdawit@gmail.com>
2022-07-28 23:36:54 +0300

    arthimetic subarray is done

--- Arthimetic SubArrays/solution.java
+++ Arthimetic SubArrays/solution.java
@@ -1,16 +1,18 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 class Solution {
     public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
-          List<Boolean> retured_list = new ArrayList<Boolean>();
+        List<Boolean> retured_list = new ArrayList<Boolean>();
+
         int check = 0;
-        // int size =
+
         for (int i = 0; i < r.length; i++) {
             int[] part = Arrays.copyOfRange(nums, l[i], r[i] + 1);
 
             Arrays.sort(part);
-            
+
             int difference = part[1] - part[0];
             for (int j = 0; j < part.length - 1; j++) {
                 if (part[j + 1] - part[j] != difference)
@@ -24,8 +26,7 @@ class Solution {
             check = 0;
 
         }
-        return retured_list;
 
-        
+        return retured_list;
     }
 }
