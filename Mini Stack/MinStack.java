bf26fa00556e685845f84c58b91e39e79e2fad4f
Dawit Andargachew <hereisdawit@gmail.com>
2022-07-31 00:06:17 +0300

    Mini Stack is done

---
+++ Mini Stack/MinStack.java
@@ -0,0 +1,42 @@
+import java.util.Stack;
+
+class MinStack {
+
+    Stack<Integer> s;
+
+    public MinStack() {
+        s = new Stack<>();
+    }
+
+    public void push(int val) {
+        s.push(val);
+    }
+
+    public void pop() {
+        if (!s.empty())
+            s.pop();
+
+    }
+
+    public int top() {
+        if (s.empty())
+            return -1;
+
+        return s.peek();
+
+    }
+
+    public int getMin() {
+        if (s.empty())
+            return -1;
+
+        int min = s.peek();
+        for (Integer integer : s) {
+            if (min > integer)
+                min = integer;
+        }
+
+        return min;
+    }
+
+}
