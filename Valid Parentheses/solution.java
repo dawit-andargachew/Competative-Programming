37870c9bdc138941c3a8a51fcfc8863269fab376
Dawit Andargachew <hereisdawit@gmail.com>
2022-07-30 21:15:50 +0300

    valid parentheses is done

---
+++ Valid Parentheses/solution.java
@@ -0,0 +1,42 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+   
+        if (s.length() % 2 != 0)
+            return false;
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+
+            switch (c) {
+                case '(':
+                case '[':
+                case '{':
+                    stack.push(c);
+                    break;
+                case ')':
+                    if (stack.empty() || stack.peek() != '(')
+                        return false;
+                    else
+                        stack.pop();
+                    break;
+                case ']':
+                    if (stack.empty() || stack.peek() != '[')
+                        return false;
+                    else
+                        stack.pop();
+                    break;
+                case '}':
+                    if (stack.empty() || stack.peek() != '{')
+                        return false;
+                    else
+                        stack.pop();
+                    break;
+            }
+        }
+
+        return stack.isEmpty();     
+    }
+}
