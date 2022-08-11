cf8e430f41819a41344431b1d7a9d87558ccc182
Dawit Andargachew <hereisdawit@gmail.com>
2022-08-11 22:12:40 +0300

    comment removed

--- 147. Insertion Sort List/Solution.java
+++ 147. Insertion Sort List/Solution.java
@@ -1,10 +1,10 @@
 class Solution {
     public ListNode insertionSortList(ListNode head) {
         ListNode result = new ListNode(0);
-        ListNode prev = result; // the last (largest) of the sorted list
+        ListNode prev = result; 
 
-        while (head != null) { // current inserting node
-            ListNode next = head.next; // cache next inserting node
+        while (head != null) {
+            ListNode next = head.next; 
             if (prev.val >= head.val) // `prev` >= current inserting node
                 prev = result; // move `prev` to the front
             while (prev.next != null && prev.next.val < head.val)
