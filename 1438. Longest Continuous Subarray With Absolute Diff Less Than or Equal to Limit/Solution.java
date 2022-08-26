09190e73c55af5f1c1afcaa32682f051c5562b6b
Dawit An <84455217+dawit01@users.noreply.github.com>
2022-08-26 16:02:49 +0300

    Update Solution.java

--- 1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit/Solution.java
+++ 1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit/Solution.java
@@ -12,9 +12,11 @@ class Solution {
         int start = 0;
         int len = 0;
         for (int i = 0; i < nums.length; ++i) {
+            // remove the last element until nums[i] is greater or equal
             while (!maxDq.isEmpty() && nums[i] > maxDq.peekLast())
                 maxDq.pollLast();
-
+            
+            // remove the last element until nums[i] is greater or equal
             while (!minDq.isEmpty() && nums[i] < minDq.peekLast())
                 minDq.pollLast();
 
@@ -22,14 +24,17 @@ class Solution {
             minDq.add(nums[i]);
 
             if (maxDq.peekFirst() - minDq.peekFirst() > limit) {
+                // when we increase the start pointer we should remove the first element inserted in the dequeue
                 if (maxDq.peekFirst() == nums[start])
                     maxDq.pollFirst();
 
+                // when we increase the start pointer we should remove the first element inserted in the dequeue
                 if (minDq.peekFirst() == nums[start])
                     minDq.pollFirst();
 
                 start++;
             }
+            
             len = Math.max(len, i - start + 1);
         }
         return len;
