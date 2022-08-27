aa1edc5d22c43f920500c0545ac58c53b65e254e
Dawit Andargachew <hereisdawit@gmail.com>
2022-08-27 09:39:52 +0300

    Remove K Digits

--- 402. Remove K Digits - - - - -/Solution.java
+++ 402. Remove K Digits - - - - -/Solution.java
@@ -1,21 +1,21 @@
 class Solution {
     public String removeKdigits(String num, int k) {
         int digits = num.length() - k;
-        char[] stk = new char[num.length()];
+        char[] stack = new char[num.length()];
         int top = 0;
 
         for (int i = 0; i < num.length(); ++i) {
             char c = num.charAt(i);
-            while (top > 0 && stk[top - 1] > c && k > 0) {
+            while (top > 0 && stack[top - 1] > c && k > 0) {
                 top -= 1;
                 k -= 1;
             }
-            stk[top++] = c;
+            stack[top++] = c;
         }
         // find the index of first non-zero digit
-        int idx = 0;
-        while (idx < digits && stk[idx] == '0')
-            idx++;
-        return idx == digits ? "0" : new String(stk, idx, digits - idx);
+        int index = 0;
+        while (index < digits && stack[index] == '0')
+            index++;
+        return index == digits ? "0" : new String(stack, index, digits - index);
     }
 }
