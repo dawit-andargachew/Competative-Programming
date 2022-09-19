d962cda5cfe7cf368f1055a7f86f04065395b0f1
Dawit Andargachew <hereisdawit@gmail.com>
2022-09-19 15:09:16 +0300

    1545. Find Kth Bit in Nth Binary String done

--- 1545. Find Kth Bit in Nth Binary String/solution.java
+++ 1545. Find Kth Bit in Nth Binary String/solution.java
@@ -1,13 +1,14 @@
 class Solution {
+    // reverse and invert the given binary string
     String reverse(String r) {
         char[] chars = new char[r.length()];
 
         int len = r.length() - 1;
-        for (int j = 0; j < r.length(); j++)
+        for (int j = 0; j < r.length(); j++) // reverse
             chars[len - j] = r.charAt(j);
 
         int i = 0;
-        while (i < r.length()) {
+        while (i < r.length()) { // invert
             if (chars[i] == '0')
                 chars[i] = '1';
             else
@@ -18,6 +19,7 @@ class Solution {
         return String.valueOf(chars);
     }
 
+    // main method
     public char findKthBit(int n, int k) {
 
         String bit = "0";
