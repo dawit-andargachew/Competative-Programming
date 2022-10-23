3e5c314bf4c4761dfb8aebf7b5ad43b7421ff3e2
Dawit Andargachew <hereisdawit@gmail.com>
2022-10-23 07:14:07 +0300

    948. Bag of Tokens done

---
+++ 948. Bag of Tokens/solution.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int power) {
+
+        Arrays.sort(tokens);
+        int score = 0;
+        int maxscore = 0;
+
+        int start = 0;
+        int end = tokens.length - 1;
+
+        while (start <= end) {
+            if (power >= tokens[start]) {
+                power -= tokens[start];
+                start++;
+                score++;
+                maxscore = Math.max(score, maxscore);
+            } else if (score > 0) { // try to get more power to maximize our score
+                power += tokens[end];
+                end--;
+                score--;
+            } else
+                return maxscore;
+        }
+
+        return maxscore;
+    }
+}
