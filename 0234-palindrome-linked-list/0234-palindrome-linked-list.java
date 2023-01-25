aa944c02e0a923901c28a3d6b53ac24daf304abe
Dawit An <84455217+dawit01@users.noreply.github.com>
2023-01-25 18:10:17 +0300

    Time: 15 ms (39.17%), Space: 118.6 MB (5.06%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -0,0 +1,73 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        // Let's reverse the linked list and  [copy its element to another list]
+        // riversing a linkedlist like below [ doesn't create another linkedlist] 
+        // so we need to it copy for latter comparison
+
+        ListNode dummy = new ListNode();
+        ListNode original = dummy;
+        ListNode prev = null;
+
+        while( head != null){
+            original.next = new ListNode(head.val);// create another linkedlist for later comparison
+            original = original.next;
+
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+
+        dummy = dummy.next; 
+        while( prev != null && dummy != null){// compare original and reversed lists
+            if( prev.val != dummy.val)
+                return false;
+            prev = prev.next;
+            dummy = dummy.next;
+        }
+
+        return true;
+    }
+}
+
+
+// copying all the elements on Arraylist and check whether it is a palindrom or not
+// class Solution {
+//     public boolean isPalindrome(ListNode head) {
+//         // if it has only one element, it is palindrom
+//         // if the list has more than one elmenet copy the values in list and check 
+//         // whether the it is palindrom or not
+
+//         if(head.next == null)
+//             return true;
+
+//         List<Integer> list = new ArrayList();
+
+//         while(head != null){
+//             list.add(head.val);
+//             head = head.next;
+//         }
+
+//         int start = 0, end = list.size() - 1;
+
+//         while(start < end){
+//             if(list.get(start) != list.get(end))
+//                 return false;
+//             start++;
+//             end--;
+//         }
+
+//         return true;        
+//     }
+// }
