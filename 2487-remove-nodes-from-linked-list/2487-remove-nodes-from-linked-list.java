a865da2a304b11e20203da58e729f68260ac83b4
Dawit An <84455217+dawit01@users.noreply.github.com>
2023-02-03 16:53:29 +0300

    Time: 185 ms (22.94%), Space: 62.2 MB (84.81%) - LeetHub

---
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNodes(ListNode head) {
+
+        // lets use stack 
+        // step-1, when we get head.val < stack.peek()  => streactly decreasing add to stack
+        // step-2, other wise remove all elements less than head.val and push(head.val)
+        // step-3, copy elements of stack in to ListNode
+            // it can handle [1,1,1,1] => all handled in step-2
+            //      to be removed from stack it must be stack.peek() < head.val
+            //      but 1 < 1 is false so only stack.push(1) is executed
+
+        Stack<Integer> stack = new Stack();
+
+        while(head != null){
+            if(stack.size() > 0 && head.val < stack.peek())// step-1
+                stack.push(head.val);
+            else {
+                while( stack.size() > 0 && stack.peek() < head.val)// step-2
+                    stack.pop();
+
+                stack.push(head.val);// append value after removing elements
+            }
+            head = head.next;
+        }
+
+        // step-3
+       ListNode dummy = new ListNode();
+       head = dummy;
+
+       for(int i = 0; i < stack.size(); i++){
+            head.next = new ListNode(stack.get(i));
+            head = head.next;
+        }
+
+       return dummy.next;        
+    }
+}
