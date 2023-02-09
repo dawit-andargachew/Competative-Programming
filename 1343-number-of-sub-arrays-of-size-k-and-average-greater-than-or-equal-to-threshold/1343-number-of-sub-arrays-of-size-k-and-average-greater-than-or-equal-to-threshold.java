8d821c63de890678e8306217a969d1a802b5204c
Dawit An <84455217+dawit01@users.noreply.github.com>
2023-02-09 17:41:46 +0300

    Time: 3 ms (89.47%), Space: 52.8 MB (95.11%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
@@ -1,48 +1,48 @@
-class Solution {
-    public int numOfSubarrays(int[] arr, int k, int threshold) {
-
-// OPTION I, OPTION II  is commented below this solution
-        int count = 0;
-        int sum = 0;
-        for(int i = 0; i < k -1; i++)
-            sum += arr[i];
-
-        for(int i = 0; i < arr.length - k + 1; i++) {
-            sum += arr[i+k -1];
-            if (sum / k >= threshold)
-                count++;
-            
-            sum -= arr[i];// decrease the sum
-        }
-
-        return count;
-    }
-}
-
-
-// OPTION II
 // class Solution {
 //     public int numOfSubarrays(int[] arr, int k, int threshold) {
 
+// // OPTION I, OPTION II  is commented below this solution
 //         int count = 0;
 //         int sum = 0;
-//         int low = -1;
-
-//         int i = 0;
-//         while (i < arr.length) {
+//         for(int i = 0; i < k -1; i++)
 //             sum += arr[i];
-//             if (i - low == k) {
-//                 if (sum / k >= threshold)
-//                     count++;
 
-//                 i++;
-//                 low++;// increase low pointer
-//                 sum -= arr[low];// decrease the sum
-
-//             } else
-//                 i++;
+//         for(int i = 0; i < arr.length - k + 1; i++) {
+//             sum += arr[i+k -1];
+//             if (sum / k >= threshold)
+//                 count++;
+            
+//             sum -= arr[i];// decrease the sum
 //         }
 
 //         return count;
 //     }
 // }
+
+
+// OPTION II
+class Solution {
+    public int numOfSubarrays(int[] arr, int k, int threshold) {
+
+        int count = 0;
+        int sum = 0;
+        int low = -1;
+
+        int i = 0;
+        while (i < arr.length) {
+            sum += arr[i];
+            if (i - low == k) {
+                if (sum / k >= threshold)
+                    count++;
+
+                i++;
+                low++;// increase low pointer
+                sum -= arr[low];// decrease the sum
+
+            } else
+                i++;
+        }
+
+        return count;
+    }
+}
