9da855210c53d9399777654b0805044853925c2c
Dawit An <84455217+dawit01@users.noreply.github.com>
2023-02-11 16:47:25 +0300

    Time: 1 ms (100.00%), Space: 42.6 MB (96.56%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+
+    //How do we check a given number is a pivot index,
+    // lets store the total sum, and
+    // if(total - prefixSum - nums[i] == prefixSum) => i is the prefixIndex
+    //Because 
+    ///         prefixSum is the sum from index i to i -1
+        int total = 0;
+        for(int i: nums)
+            total += i;
+        
+        int prefixSum =0;
+        int i = 0;
+       while(i < nums.length){
+            if(total - prefixSum - nums[i] == prefixSum)
+                return i;
+            else
+                prefixSum += nums[i++];
+        }
+
+        return -1;        
+    }
+}
