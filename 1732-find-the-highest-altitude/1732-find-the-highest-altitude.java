4859dc798db04a3cbf9640acad10c08ee7b16fd6
Dawit An <84455217+dawit01@users.noreply.github.com>
2023-02-12 23:41:28 +0300

    Time: 0 ms (100.00%), Space: 40.1 MB (45.98%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -1,11 +1,16 @@
 class Solution {
     public int largestAltitude(int[] gain) {
-        int max = 0;
+
+        // lets user prefix sum, and store each score and compare it with the previsou max value
+        int max =0;
         int sum = 0;
-        for(int i : gain){
-            sum += i;
-            max = Math.max(sum,max);
+        int i = 0;
+        while( i < gain.length){
+            sum += gain[i];
+            max = Math.max(max, sum);
+            i++;
         }
-        return max;        
+
+        return max;
     }
 }
