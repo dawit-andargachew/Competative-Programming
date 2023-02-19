43eb58da6e32c350b61bfaf0980285b27f48dfc8
Dawit An <84455217+dawit01@users.noreply.github.com>
2023-02-19 09:22:13 +0300

    Time: 57 ms (65.20%), Space: 42.8 MB (72.30%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -5,16 +5,16 @@ class Solution {
     // and we can keep track of weather they are duplicate or not
     Stack<Character> stack = new Stack();
       for(char c : s.toCharArray()){
-
-            if(!stack.isEmpty() && stack.peek() == c)
-                stack.pop();
-            else
-                stack.add(c);
+        if(stack.size() > 0 && stack.peek() == c)
+            stack.pop();
+         else
+            stack.add(c);
         }
 
-        char[] a = new char[stack.size()];
+        // char[] a = new char[stack.size()];
+        StringBuilder a = new StringBuilder();
         for(int i = 0; i < stack.size(); i++)
-            a[i] = stack.get(i);
+            a.append(stack.get(i));
 
         return String.valueOf(a);
     }
