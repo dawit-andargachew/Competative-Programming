e1f1474b952bdda76416aab9bb23cc462a2b876d
Dawit An <84455217+dawit01@users.noreply.github.com>
2023-02-23 02:36:14 +0300

    Time: 41 ms (67.13%), Space: 67.6 MB (11.64%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
@@ -9,57 +9,27 @@
  * }
  */
 class Solution {
-  public int[] nextLargerNodes(ListNode head) {
-     // lets use two Stacks Key and value, K stores each element and V holds the next greater value
-     // Step-1 if head.val >= head.next.val, it is not strictly increasing so store (head.val, 0)
-     // Step-2, head.val < head.next.val, it is strictly increasing 
-     //    and we need to check whether it is the next greater element for previos elements l
-     //    like [1,7,3,2,1,5] => (1,3),(3,0),(2,0)
-     //     after we get 5 it becomes the next greter element for 3 and 2 as well [not for (1,7) it has next greater element]
-     //   so traverse backward and 
-     ///       1, if(K.get(i) > head.val) =>  head.val is not greater element for this and all the previos values found before it
-     //                   take (1,7,3,2,1,5), 5 is greaer element for 3,2,1 but not for 1,7
-     //                   because 7 > 5 => 5 is not greater element for 7 and previos elements of 7 i.e, [1,7]
-     //       2, else if(V.get(i) == 0) => head.val is greater element
-     //                        take (1,7,3,2,1,5),  for 3,2 have no greater element [and are less than 5]
-     //                          if they were like 8 or 9 => (1,7,8,3,2,1,5) [ gteater than 5], the above condition break the loop
-     // Step-3 if (head.next == null) => have no greaer element so make 0 [ for last elements]
-
-    Stack < Integer > K = new Stack();
-    Stack < Integer > V = new Stack();
-    while (head != null) {
-
-      if (head.next != null && head.val >= head.next.val) { // step-1 Not strictly increasing
-        K.add(head.val);
-        V.add(0);
-
-      } else if (head.next != null && head.val < head.next.val) { // step-2 is strictly increasing
-
-        // traverse backward and check it is their next greater element
-        for (int i = K.size() - 1; i >= 0; i--) {
-          if (K.get(i) >= head.next.val)
-            break;
-
-          if (V.get(i) == 0)// if is is zero they have no greater element, but if not zero they have next greater element
-            V.set(i, head.next.val);
+    public int[] nextLargerNodes(ListNode head) {
+        // here is a monotonic stack similar to 
+        //      1475. Final Prices With a Special Discount in a Shop
+        //      496. Next Greater Element I
+        List<Integer> list = new ArrayList();        
+        while( head != null){
+            list.add(head.val);
+            head = head.next;
         }
-
-        K.add(head.val);
-        V.add(head.next.val);
-
-      } else if (head.next == null) {// step-3 next element is null
-        K.add(head.val);
-        V.add(0);
-      }
-
-      head = head.next;
+        
+        int[] answer = new int[list.size()];
+        Stack<Integer> stack = new Stack();
+        
+        for(int i = 0; i < list.size(); i++){
+            
+            while( stack.size() > 0 && list.get(stack.peek()) < list.get(i) )
+                answer[stack.pop()] = list.get(i);
+            
+            stack.push(i);
+        }
+        
+        return answer;        
     }
-
-    //array to be returned
-    int[] nums = new int[V.size()];
-    for (int i = 0; i < V.size(); i++)
-      nums[i] = V.get(i);
-
-    return nums;
-  }
 }
