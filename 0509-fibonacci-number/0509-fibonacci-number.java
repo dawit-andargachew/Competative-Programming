e66fc2441969d7521ed2a6d8deb475a0cdf45cf6
Dawit An <84455217+dawit01@users.noreply.github.com>
2023-03-02 11:30:06 +0300

    Time: 9 ms (30.99%), Space: 39.6 MB (20.34%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,20 +1,11 @@
 class Solution {
     public int fib(int n) {
+        if( n == 0)
+            return 0;
+        else if( n == 1)
+            return 1;
         
-    if( n == 0)
-        return 0;
-    else if( n == 1)
-        return 1;
+        return fib(n-2) + fib(n -1);
         
-       int[] num = new int[n+1];
-        num[1] = 1;
-        
-        int i  = 2;
-        while( i <= n){
-            num[i] = num[i-1] + num[i-2];            
-            i++;
-        }
-        
-        return num[n];
     }
 }
