3ccc4fb408a5e19e468aadb92dd2b1362002f4fa
Dawit An <84455217+dawit01@users.noreply.github.com>
2023-03-12 14:54:47 +0300

    Time: 39 ms (42.15%), Space: 14.1 MB (77.67%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -4,19 +4,44 @@ class Solution:
         acc = []
 
         def backtrack(idx):
+
+            #doesn't need any condition all of them are valid candidates
+            ans.append(acc[:])
             
-            # valid candidates
-            if len(acc) <= len(nums):
-                ans.append(acc[:])
-            
-            if idx > len(nums):
+            if idx >= len(nums):
                 return
 
-            # generate possible candidates
-            for i in range(idx, len(nums) ):
+            # valid candidates
+            for i in range(idx, len(nums) ): 
                 acc.append( nums[i] )
                 backtrack(i + 1)
                 acc.pop()
 
         backtrack(0)
         return ans
+# SEE ALSO THE CODE BELOW HERE WITHOUT PRUNNING
+
+# with out prunning generate all posible outcomes and check the condition 
+
+# class Solution:
+#     def subsets(self, nums: List[int]) -> List[List[int]]:
+#         ans = []
+#         acc = []
+
+#         def backtrack(i):
+#             # valid candidates
+#             if i == len(nums):
+#                 ans.append(acc[:])
+            
+#             if i >= len(nums):
+#                 return
+
+#             # generate possible candidates
+#             acc.append( nums[i] )
+#             backtrack(i + 1)
+#             acc.pop()
+#             backtrack(i + 1)
+
+#         backtrack(0)
+#         print(ans)
+#         return ans
