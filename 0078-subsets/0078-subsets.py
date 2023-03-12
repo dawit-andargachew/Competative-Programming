f6ac90c0aa90c86dad18cdd0ee8888e515f49f51
Dawit An <84455217+dawit01@users.noreply.github.com>
2023-03-12 10:53:58 +0300

    Time: 42 ms (23.51%), Space: 14.1 MB (27.66%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,24 +1,22 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        
         ans = []
         acc = []
-        
+
         def backtrack(idx):
             
-            if len(acc) <= idx:
+            # valid candidates
+            if len(acc) <= len(nums):
                 ans.append(acc[:])
-                
+            
             if idx > len(nums):
                 return
-            
-            for i in range( idx, len(nums) ):
-                
-                if nums[i] not in acc:
-                    acc.append( nums[i] )
-                    backtrack(i + 1)
-                    acc.pop()
-        
-        
+
+            # generate possible candidates
+            for i in range(idx, len(nums) ):
+                acc.append( nums[i] )
+                backtrack(i + 1)
+                acc.pop()
+
         backtrack(0)
         return ans
