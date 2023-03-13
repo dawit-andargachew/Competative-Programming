4221a6abcd41b5eeef44d05960c0330c492c14bc
Dawit An <84455217+dawit01@users.noreply.github.com>
2023-03-13 23:25:27 +0300

    Time: 533 ms (86.29%), Space: 27.9 MB (40.29%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
@@ -1,22 +1,27 @@
 class Solution:
     def dividePlayers(self, skill: List[int]) -> int:
         
+        # sort to group players
         skill.sort()
         l,r = 0, len(skill) - 1
         
-        player = skill[l] + skill[r]
-        total = skill[l] * skill[r]
+        # sum of player
+        player_sum = skill[l] + skill[r]
+        
+        # sum of total_chemistry of all groups
+        total_chemistry = skill[l] * skill[r]
         l += 1
         r -= 1
+        
         while l < r:
-            temp = skill[l] + skill[r]
-            if temp == player:
-                total += skill[l] * skill[r]
+            temp_sum = skill[l] + skill[r]
+            if temp_sum == player_sum:
+                total_chemistry += skill[l] * skill[r]
             else:
-                total = -1
+                total_chemistry = -1
                 break
             
             l += 1
             r -= 1          
         
-        return total
+        return total_chemistry
