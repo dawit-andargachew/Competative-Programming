7cd1521fcd33626f361d56bf07d3e0ceeed3d926
Dawit An <84455217+dawit01@users.noreply.github.com>
2023-03-20 13:07:19 +0300

    Time: 48 ms (5.21%), Space: 13.8 MB (58.33%) - LeetHub

---
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isCompleteTree(self, root: Optional[TreeNode]) -> bool:
+        
+        # a complete binary tree is filled from left to right, so each node needs two things
+        # 1, does the left and right sub-tree are complete
+        # 2, left shallowest and right deepest nodes should differ at most by one
+        # meaning,
+        #       left - right <= 1 and left >= right => so we can say it is definately complete binary tree
+
+        # each node returns three things
+        # [is_sub_tree_complete, deepest, shallowet]
+
+        def check_complete(root):
+            
+            # [ is_complete, deepest, shallow]
+            if not root:
+                return [True, 0, 0]
+            
+            if not root.left and not root.right:
+                return [True, 1, 1]
+            
+            left = check_complete(root.left)
+            right = check_complete(root.right)
+
+            is_complete = left[0] and right[0] and left[2] >= right[1] and ( left[2] - right[1] <= 1 )
+            max_depth = max(left[1], right[1]) + 1
+            min_depth = min(left[2], right[2]) + 1 
+
+            return [is_complete,max_depth, min_depth]
+
+        result = check_complete(root)
+
+        return result[0] and result[1] - result[2] <= 1
