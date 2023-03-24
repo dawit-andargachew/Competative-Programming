3ed0ec7f22a8f9378b4d3d334a13adeed17ca68f
Dawit An <84455217+dawit01@users.noreply.github.com>
2023-03-24 10:22:24 +0300

    Time: 210 ms (29.70%), Space: 43.3 MB (5.00%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,5 +3,13 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        s.reverse()
-        
+
+        def reverse_func(left,right):
+            if left >= right:
+                return
+            # make a swap to reverse strings
+            s[left], s[right] = s[right], s[left]
+            
+            reverse_func(left + 1, right - 1)
+
+        reverse_func(0, len(s)-1)
