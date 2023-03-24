6b45c8524c010be90e38d65cdaf4a70b1bd89b8b
Dawit An <84455217+dawit01@users.noreply.github.com>
2023-03-25 00:56:09 +0300

    Time: 1061 ms (22.73%), Space: 37.3 MB (6.59%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,20 +1,15 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
 
-        if len(nums) == 1:
-            return nums[0]
-
         # apply quick sort and in every part check wheather the kth number
-        k_thLargest = -1
+        result = -1
         def Quick(start, end):
-
-            nonlocal k_thLargest
+            nonlocal result
 
             pivot = nums[start]
             write = start + 1
 
             for read in range(start + 1, end + 1):
-
                 if pivot >= nums[read]:
                     nums[write], nums[read] = nums[read], nums[write]
                     write += 1 
@@ -22,7 +17,7 @@ class Solution:
             nums[start], nums[write - 1] = nums[write - 1], nums[start]
 
             if len(nums) - ( write - 1) == k :
-                k_thLargest = nums[write - 1]
+                result = nums[write - 1]
                 return
 
             elif len(nums) - ( write - 1) < k:
@@ -32,4 +27,4 @@ class Solution:
 
         Quick(0, len(nums) - 1)
         
-        return k_thLargest
+        return result
