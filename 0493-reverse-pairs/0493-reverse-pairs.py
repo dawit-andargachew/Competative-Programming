30a6f3de5c4a17a088cf0474fc945d98e1d0a9c4
Dawit An <84455217+dawit01@users.noreply.github.com>
2023-03-31 10:26:19 +0300

    Time: 2460 ms (26.95%), Space: 28 MB (5.99%) - LeetHub

---
+++ 0493-reverse-pairs/0493-reverse-pairs.py
@@ -0,0 +1,54 @@
+class Solution:
+    def reversePairs(self, nums: List[int]) -> int:
+
+        pairs = 0
+        # it counts reverse pairs and return sorted list
+        # sorting the list makes easy to count reverse pairs
+        def Combine(left, right):
+            nonlocal pairs
+            
+            # since the sub-array are sorted, it makes easy to find reverse pairs
+            i, j = 0, 0
+            while i < len(left) and j < len(right):
+
+                if left[i] > 2 * right[j]:
+                    pairs += len(left) - i
+                    j += 1
+                else:
+                    i += 1
+
+            # sort left and right before returnig to higher level callers
+            merged, i , j = [], 0 , 0
+            while i < len(left) and j < len(right):
+                if left[i] <= right[j]:
+                    merged.append( left[i])
+                    i += 1
+                else:
+                    merged.append( right[j] )
+                    j += 1
+
+            # for left part
+            while i <len(left):
+                merged.append( left[i] )
+                i += 1
+            
+            # for right part
+            while j < len(right):
+                merged.append( right[j] )
+                j += 1            
+            return merged
+
+
+        # split each sub-array in to two until one element remains
+        def split_into_Half(l,r):
+            if l == r:
+                return [nums[l]]
+            
+            mid = ( l + r)//2
+
+            left = split_into_Half (l, mid)
+            right = split_into_Half (mid + 1, r)
+            return Combine(left,right)
+        
+        split_into_Half(0, len(nums) - 1)
+        return pairs
