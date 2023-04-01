86959d03970182922ebf1e11f874ab52466c35fa
Dawit An <84455217+dawit01@users.noreply.github.com>
2023-04-01 22:49:03 +0300

    Update Cirno's Perfect Bitmasks Classroom.py

--- Cirno's Perfect Bitmasks Classroom.py
+++ Cirno's Perfect Bitmasks Classroom.py
@@ -1,12 +1,9 @@
-test_case = int(input())
-
-for _ in range(test_case):
+for _ in range( int(input()) ):
    
     n = int(input())
 
     if n == 1:
         print(3)
-
     elif n & (n-1) == 0:
         print(n + 1)
     else:
