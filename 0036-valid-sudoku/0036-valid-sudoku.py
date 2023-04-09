dc0f9bca3a7e214563a7d26daffcb8a61c730b78
Dawit An <84455217+dawit01@users.noreply.github.com>
2023-04-09 11:57:04 +0300

    Time: 82 ms (99.33%), Space: 13.9 MB (23.01%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,59 +1,29 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
 
-       # validate rows and columns
-        for row in board: # validate row
-            a = [ r for r in row if r.isdigit()]
-            if len(a) != len(set(a)):
+        # validate rows and columns
+        for b in board: # validate row
+            row = [ r for r in b if r.isdigit()]
+            if len(row) != len(set(row)):
                 return False
-        
-        for j in range( len(board)): # validate column
-            col = [row[j] for row in board if row[j].isdigit()]
 
+        for j in range(9): # validate column
+            col = [row[j] for row in board if row[j].isdigit()]
             if len(col) != len(set(col)):
                 return False
-
+        
         # validate each 3x3 matrixes
         r = 0
-        while r < len(board):
-            one, two, three = [], [], []
-
-            # one = [ for c in board[r]]
-            
-            # first 3 * 3
-            j, i = 0, 0
+        while r < 9:
+            one, two , three = [], [], []
+            i = 0
             while i < 3:
-                while j < 3:
-                    if board[r + i][j].isdigit():
-                        one.append(board[r + i][j])
-                    j += 1
-                i, j = i + 1, 0
-            if len(one) != len(set(one)):
+                one += [c for c in board[r + i][0:3] if c.isdigit()]
+                two += [c for c in board[r + i][3:6] if c.isdigit()]
+                three += [c for c in board[r + i][6:9] if c.isdigit()]
+                i += 1
+            if len(one) != len(set(one)) or len(two) != len(set(two)) or len(three) != len(set(three)):
                 return False
-
-            # second 3 * 3
-            j, i = 3, 0
-            while i < 3:
-                while j < 6:
-                    if board[r + i][j].isdigit():
-                        two.append(board[r + i][j])
-                    j += 1
-                i, j = i + 1, 3
-            if len(two) != len(set(two)):
-                return False
-            
-            # third 3 * 3
-            j, i = 6, 0
-            while i < 3:
-                while j < 9:
-                    if board[r + i][j].isdigit():
-                        three.append(board[r + i][j])
-                    j += 1
-                i, j = i + 1, 6
-            if len(three) != len(set(three)):
-                return False
-
             r += 3
 
-        return True
-    
+        return True 
