69c8aef5925f7ba6b3462691cdf4916cd28f7aad
Dawit An <84455217+dawit01@users.noreply.github.com>
2023-04-09 11:55:07 +0300

    Time: 102 ms (34.37%), Space: 13.9 MB (70.39%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+
+        # validate rows and columns
+        for b in board: # validate row
+            row = [ r for r in b if r.isdigit()]
+            if len(row) != len(set(row)):
+                return False
+
+        for j in range(9): # validate column
+            col = [row[j] for row in board if row[j].isdigit()]
+            if len(col) != len(set(col)):
+                return False
+        
+        # validate each 3x3 matrixes
+        r = 0
+        while r < 9:
+            one, two , three, i= [], [], [], 0
+            while i < 3:
+                one += [c for c in board[r + i][0:3] if c.isdigit()]
+                two += [c for c in board[r + i][3:6] if c.isdigit()]
+                three += [c for c in board[r + i][6:9] if c.isdigit()]
+                i += 1
+            if len(one) != len(set(one)) or len(two) != len(set(two)) or len(three) != len(set(three)):
+                return False
+            r += 3
+
+        return True 
