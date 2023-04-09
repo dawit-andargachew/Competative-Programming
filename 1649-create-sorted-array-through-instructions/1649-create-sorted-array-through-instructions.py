f23cf72c6f58872a32703a91ada976b1fe47ca16
Dawit An <84455217+dawit01@users.noreply.github.com>
2023-04-09 19:08:22 +0300

    Time: 5434 ms (69.09%), Space: 29.9 MB (43.03%) - LeetHub

---
+++ 1649-create-sorted-array-through-instructions/1649-create-sorted-array-through-instructions.py
@@ -0,0 +1,18 @@
+from sortedcontainers import SortedList
+class Solution:
+    def createSortedArray(self, instructions: List[int]) -> int:
+        
+        mod = 10**9 + 7
+        s = SortedList()
+        answer = 0
+        
+        # bisect_left and bisect_right return the leftmost and rightmost index where the value can be inserted without
+        # changing the order of elements. If the value does not exist in the list, they both return the same index. 
+        # The difference arises when the value exists in the list. 
+        for i in instructions:
+            numberOfLess = s.bisect_left(i)
+            numberOfGreater =  len(s) - s.bisect_right(i)
+            answer += min(numberOfLess, numberOfGreater)
+            s.add(i)
+        
+        return answer % mod
