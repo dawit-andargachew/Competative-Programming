ec7c818aaacdae832d75140844f4456c76f751a6
Dawit An <84455217+dawit01@users.noreply.github.com>
2023-04-10 23:44:11 +0300

    Time: 716 ms (12.50%), Space: 26.3 MB (14.71%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,50 +1,27 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
 
-        perimeter = 0
+        # perimeter = 0
         visited, directions = set(), [(0,1), (0,-1), (1,0), (-1,0)]
-
-        def getFirstOne():
-            startRow, startCol, row , col = 0, 0, 0, 0
-            
-            while row < len(grid):
-                while col < len(grid[0]):
-                    if grid[row][col] == 1:
-                        startRow, startCol = row, col
-                        row, col = len(grid), len(grid[0]) # breaks from the loop after getting the first one
-                    col += 1
-                col = 0 # reset column for the next row
-                row += 1
-
-            return [startRow, startCol]
-
-        def isInValid(row, col):
-            return row >= len(grid) or row < 0 or col >= len(grid[0]) or col < 0
-
         def dfs(row, col):
-            nonlocal perimeter
 
             # base cases
-            if isInValid(row, col) or grid[row][col] == 0:
-                perimeter += 1
-                return
-            
-            # calculate perimeter
-            # for d in directions:
-            #     newRow, newCol = row + d[0], col + d[1]
-            #     if isInValid(newRow, newCol):
-            #         perimeter += 1
-            #     elif grid[newRow][newCol] == 0:
-            #         perimeter += 1
+            if row >= len(grid) or row < 0 or col >= len(grid[0]) or col < 0 or grid[row][col] == 0:
+                return 1
+            if (row, col) in visited:
+                return 0
 
             visited.add( ( row, col))
+
+            perimeter = 0
             for d in directions:
                 newRow, newCol = row + d[0], col + d[1]
-                if (newRow, newCol) not in visited:
-                    dfs(newRow, newCol)
-
+                perimeter += dfs(newRow, newCol)
+            
+            return perimeter
+        
         # lets get the first 1 in the matrix and start find the perimeter over that
-        startRow, startCol = getFirstOne()
-        dfs(startRow, startCol)
-
-        return perimeter
+        for i in range( len(grid)):
+            for j in range( len(grid[0])):
+                if grid[i][j] == 1:
+                    return dfs(i, j)
