e48cadb898bfdf6d7e0ee54b4c1f1b3dd886a99a
Dawit An <84455217+dawit01@users.noreply.github.com>
2023-04-27 18:10:43 +0300

    Time: 60 ms (22.95%), Space: 16.5 MB (7.65%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -9,31 +9,30 @@ class Solution:
                 return False
             return True
         
-        time, visited, q, initialOnes, finalOnes = 0, set(), deque(), 0, 0
+        minute, q, initialOnes = 0, deque(), 0
         # It is multi Source BFS so take all 'rotten' oranges on the queue
         for r in range( len(grid)):
             for c in range( len(grid[0])):
                 if grid[r][c] == 2:
                     q.append( [r,c] )
-                    visited.add( (r,c) )
                 if grid[r][c] == 1:
                     initialOnes += 1
 
         # calculate the time needed to rotten all oranges
         directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
         while q:
-            size, FreshOrangeOnThisLevel = len(q), 0
+            size = len(q)
             for _ in range(size):
                 row, col = q.popleft()
                 for r, c in directions:
                     newRow, newCol = row + r, col + c
-                    if (newRow, newCol) not in visited and isValid(newRow, newCol) and grid[newRow][newCol] == 1:
+                    if isValid(newRow, newCol) and grid[newRow][newCol] == 1:
+                        grid[newRow][newCol] = 2
                         q.append( [newRow, newCol])
-                        visited.add( (newRow, newCol))
-                        FreshOrangeOnThisLevel, finalOnes = FreshOrangeOnThisLevel + 1, finalOnes + 1
+                        initialOnes -= 1
 
-            if FreshOrangeOnThisLevel > 0:
-                time += 1
+            if len(q) > 0:
+                minute += 1
         
-        # After finishing BFS, initial and final fresh Oranges should be the same
-        return time if initialOnes == finalOnes else - 1
+        # After finishing BFS, initial fresh Oranges should be zero
+        return minute if initialOnes == 0 else - 1
