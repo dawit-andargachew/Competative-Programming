bff8899fb4e5bbd714b9743845776e3e09811522
Dawit An <84455217+dawit01@users.noreply.github.com>
2023-05-18 13:47:05 +0300

    Time: 48 ms (56.25%), Space: 16.5 MB (11.57%) - LeetHub

---
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
@@ -0,0 +1,35 @@
+class Solution:
+    def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
+        
+        # initialize representative dict, 
+        # initially, all chars are their own representative
+        representative = {i : i for i in s1}
+        representative |= {i: i for i in s2}
+        representative |= {i: i for i in baseStr}
+
+        def union(x, y):
+            x_rep = find(x)
+            y_rep = find(y)
+            
+            # to make it lexicographically smallest check the reps
+            if x_rep >= y_rep:
+                representative[ x_rep ] = y_rep
+            else:
+                representative[ y_rep ] = x_rep
+        
+        def find(val):
+            while val != representative[val]:
+                val = representative[val]
+            return val
+        
+
+        for i in range( len(s1) ):
+            union( s1[i], s2[i])
+        
+        # extract lexicographically smallest equivalent string for baseStr
+        answer =  ""
+        for i in baseStr:
+            # for each char find its representative
+            answer += find(i)
+
+        return answer
