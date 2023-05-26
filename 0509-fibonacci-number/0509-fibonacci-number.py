2e109855525d74a0c841af40b1f9ac6644b219fb
Dawit An <84455217+dawit01@users.noreply.github.com>
2023-05-26 17:48:44 +0300

    Time: 42 ms (55.59%), Space: 16.2 MB (41.23%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -4,7 +4,7 @@ class Solution:
             return n
         
         memo = [0] * (n + 1)
-        memo[0], memo[1]  = 0, 1
+        memo[1]  = 1
         
         for i in range(2, n + 1):
             memo[i] = memo[i - 1] + memo[i - 2]          
