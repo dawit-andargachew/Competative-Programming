d69e5fb8a3e64c558763a5c8c795a50d20f05692
Dawit An <84455217+dawit01@users.noreply.github.com>
2023-07-17 14:06:43 +0300

    Time: 5133 ms (53.49%), Space: 32 MB (34.88%) - LeetHub

--- 1649-create-sorted-array-through-instructions/1649-create-sorted-array-through-instructions.py
+++ 1649-create-sorted-array-through-instructions/1649-create-sorted-array-through-instructions.py
@@ -6,9 +6,12 @@ class Solution:
         s = SortedList()
         answer = 0
         
-        # bisect_left and bisect_right return the leftmost and rightmost index where the value can be inserted without
-        # changing the order of elements. If the value does not exist in the list, they both return the same index. 
+        # bisect_left and bisect_right return the leftmost and rightmost
+        # index where the value can be inserted without
+        # changing the order of elements. If the value does not exist in the list, 
+        # they both return the same index. 
         # The difference arises when the value exists in the list. 
+    
         for i in instructions:
             numberOfLess = s.bisect_left(i)
             numberOfGreater =  len(s) - s.bisect_right(i)
