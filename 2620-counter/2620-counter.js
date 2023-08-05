28f053cccf03358ff002a33e5660505aec5bfbe9
Dawit An <84455217+dawit01@users.noreply.github.com>
2023-08-05 14:57:02 +0300

    Time: 45 ms (95.72%), Space: 42.2 MB (24.39%) - LeetHub

---
+++ 2620-counter/2620-counter.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    var val = n - 1
+    return function() {
+        val += 1
+        return val
+        
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
