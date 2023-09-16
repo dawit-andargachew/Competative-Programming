4a86a3c420eb528aa47a3be9ad6c4586e9185edb
Dawit Andargachew <hereisdawit@gmail.com>
2023-09-16 21:19:08 +0300

    Time: 514 ms (90.13%), Space: 21 MB (41.45%) - LeetHub

---
+++ 1705-maximum-number-of-eaten-apples/1705-maximum-number-of-eaten-apples.py
@@ -0,0 +1,15 @@
+from heapq import heappush, heappop
+class Solution:
+    def eatenApples(self, A: List[int], D: List[int]) -> int:
+        ans, idx, N = 0, 0, len(A)
+        h = []
+        while idx < N or h:
+            if idx < N and A[idx] > 0:
+                heappush(h, [idx + D[idx], A[idx]])
+            while h and (h[0][0] <= idx or h[0][1] <= 0):
+                heappop(h)
+            if h:
+                h[0][1] -= 1
+                ans += 1
+            idx += 1
+        return ans 
