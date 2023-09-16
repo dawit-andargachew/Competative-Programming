1c2bbe9088d4f3b09c3aaea474942a3f791c82cf
Dawit Andargachew <hereisdawit@gmail.com>
2023-09-16 22:15:39 +0300

    Time: 522 ms (85.53%), Space: 20.9 MB (56.58%) - LeetHub

--- 1705-maximum-number-of-eaten-apples/1705-maximum-number-of-eaten-apples.py
+++ 1705-maximum-number-of-eaten-apples/1705-maximum-number-of-eaten-apples.py
@@ -1,14 +1,15 @@
 class Solution:
     def eatenApples(self, A: List[int], D: List[int]) -> int:
+        
         ans, idx, N = 0, 0, len(A)
-        h = []
-        while idx < N or h:
+        height = []
+        while idx < N or height:
             if idx < N and A[idx] > 0:
-                heappush(h, [idx + D[idx], A[idx]])
-            while h and (h[0][0] <= idx or h[0][1] <= 0):
-                heappop(h)
-            if h:
-                h[0][1] -= 1
+                heappush(height, [idx + D[idx], A[idx]])
+            while height and (height[0][0] <= idx or height[0][1] <= 0):
+                heappop(height)
+            if height:
+                height[0][1] -= 1
                 ans += 1
             idx += 1
             
