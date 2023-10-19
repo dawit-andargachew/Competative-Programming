cc00803186dd14f04d4ccb9325320338a80dbaac
Dawit Andargachew <hereisdawit@gmail.com>
2023-10-19 15:59:15 +0300

    Time: 757 ms (14.64%), Space: 79.1 MB (5.35%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.py
@@ -0,0 +1,62 @@
+class LinkedNode:
+    def __init__(self, key = -1, val = -1):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = dict()
+        self.head = LinkedNode()
+        self.tail = LinkedNode()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        
+        return self.__moveToEnd(key).val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.__delete(key)
+        self.__create(key, value)
+        
+        if len(self.cache) > self.capacity:
+            self.__delete(self.head.next.key)
+
+    def __create(self, key, val) -> LinkedNode: 
+        node = LinkedNode(key, val)
+        self.__addToEnd(node)
+        self.cache[key] = node
+
+        return node
+    
+    def __delete(self, key) -> None:
+        deleteNode = self.__evict(key)
+        del deleteNode
+        del self.cache[key]
+    
+    def __moveToEnd(self, key) -> LinkedNode:
+        node = self.__evict(key)
+        self.__addToEnd(node)
+        
+        return node
+
+    def __addToEnd(self, node) -> None:
+        node.prev = self.tail.prev
+        node.next = self.tail
+        self.tail.prev.next = node
+        self.tail.prev = node
+    
+    def __evict(self, key) -> LinkedNode: 
+        node = self.cache[key]
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        node.next = None
+        node.prev = None
+        
+        return node
