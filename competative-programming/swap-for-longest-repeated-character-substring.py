d8665acd07c24853584985a62603da3d3a54017c
Dawit Andargachew <dawit.andargachew.asmare@gmail.com>
2024-03-23 21:42:49 +0300

    Added solution for Swap For Longest Repeated Character Substring

--- competative-programming/swap-for-longest-repeated-character-substring.py
+++ competative-programming/swap-for-longest-repeated-character-substring.py
@@ -10,20 +10,26 @@ class Solution:
             temp[ text[i] ] += 1
             h, s = max(temp, key=temp.get), min(temp, key=temp.get)
 
-            # condition one
+            # condition one - if it has more than 3 chars remove till 2 remain
             while len(temp) > 2 and left < i:
                 temp[ text[left] ] -= 1
                 if temp[ text[left] ] == 0:
                     del temp[ text[left] ]
                 left += 1
 
-            # condition two - while both have frequency > 2
+            # condition two - while both have frequency > 2, remove till one of them have freq of 1
             while len(temp) > 1 and min(temp.values()) > 1 and left < i:
                 temp[ text[left] ] -= 1
                 if temp[ text[left] ] == 0:
                     temp.pop( temp[left] )
                 left += 1
             
+            '''
+            `h in temp` seems unnecessary since `temp` is defaultdict, what if h is removed in the above while
+            loops? So `h in temp` prevents new elements from inserting with freq of 1 and make sure not to increase
+            the temp size.
+            If the temp size is increased, it initiates the above while loops to remove necessary values
+            '''
             if h in temp and table[h] == temp[h] and len(temp) > 1:
                 temp[ text[left] ] -= 1
                 if temp[ text[left] ] == 0:
