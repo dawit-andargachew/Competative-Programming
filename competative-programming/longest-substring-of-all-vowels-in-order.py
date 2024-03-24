5a6a50401c9859830aa5f84bdfc935950cba85f9
Dawit Andargachew <dawit.andargachew.asmare@gmail.com>
2024-03-24 20:27:54 +0300

    Added solution for Longest Substring Of All Vowels in Order

--- competative-programming/longest-substring-of-all-vowels-in-order.py
+++ competative-programming/longest-substring-of-all-vowels-in-order.py
@@ -1,35 +1,29 @@
 class Solution:
     def longestBeautifulSubstring(self, word: str) -> int:
         
-        table = defaultdict(int)
-        table['a'] = 0
-        table['e'] = 1
-        table['i'] = 2
-        table['o'] = 3
-        table['u'] = 4
-        
+        table = {'a': 0, 'e': 1, 'i': 2, 'o': 3, 'u': 4}
         left = right = ans = 0
 
         while right < len( word ):
             
-            # get the beginning -> move left till a
+            is_there_e = False # make sure there is `e`-> take this "aiooooouuu"; it meight get skipped
+            
+            # get the beginning -> move left till `a`
             while left + 1 < len(word) and word[left] != 'a':
                 left += 1
             
+            # check wheather the elemens are consecutive
             right = left + 1            
             while right + 1 < len(word) and ( table[ word[right] ] == table[ word[right + 1] ] \
                  or table[word[right]] == table[word[right + 1]] - 1):
+                 if not is_there_e and word[right] == 'e':
+                    is_there_e = True
                  right += 1
             
-            if left < len(word) and right < len(word) and word[left] == 'a' and word[right] == 'u':
-                temp = left # make sure there is `e`-> take this "aiooooouuu"
-                while temp < right and word[temp] != "e":
-                    temp += 1
-                
-                if word[temp] == 'e':
-                    ans = max(ans, right - left + 1)
+            if is_there_e and left < len(word) and right < len(word) and  word[left] == 'a' and word[right] == 'u':
+                ans = max(ans, right - left + 1)
 
-            left = right
-            right += 1
+            left, right = right, right + 1
+        
         # a,e,i,o,u
         return ans if ans > 4 else 0
