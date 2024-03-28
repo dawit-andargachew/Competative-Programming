612523bae0c320d35e049fa3f5d14a8e52b033fe
Dawit Andargachew <dawit.andargachew.asmare@gmail.com>
2024-03-28 23:49:05 +0300

    Added solution for Maximum Sum of Distinct Subarrays With Length K

---
+++ Competative-Programming/maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+
+        ans = left = total = 0
+        s = defaultdict(int)
+        
+        for i in range(len(nums)):
+            s[ nums[i] ] += 1
+            total += nums[i]
+
+            while (s[ nums[i] ] > 1 or len(s) > k) and left < i:
+                s[ nums[left] ] -= 1
+                total -= nums[left]
+                
+                if s[ nums[left] ] == 0:
+                    s.pop( nums[left] )
+                left +=1
+
+            if len(s) == k and i - left + 1 == k:
+                ans = max(ans, total)
+            
+
+        return ans
+        
