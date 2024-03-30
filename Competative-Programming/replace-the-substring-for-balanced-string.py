f18fde4a1aa51c14efa47ab204fa6c05fe5e05d7
Dawit Andargachew <dawit.andargachew.asmare@gmail.com>
2024-03-30 11:00:13 +0300

    Added solution for Replace the Substring for Balanced String

--- Competative-Programming/replace-the-substring-for-balanced-string.py
+++ Competative-Programming/replace-the-substring-for-balanced-string.py
@@ -1,39 +1,30 @@
 class Solution:
     def balancedString(self, s: str) -> int:
         
-        table = defaultdict(int)
+        extra = defaultdict(int)
         for i in ['Q','W','E','R']:
-            extra = s.count(i) - len(s)//4
-            table[i] = extra if extra > 0 else 0
+            dif = s.count(i) - len(s)//4
+            extra[i] = dif if dif > 0 else 0
 
         # no need to replace
-        if table["Q"] == table["W"] == table["R"] == table["E"] == len(s)//4:
+        if extra["Q"] == extra["W"] == extra["R"] == extra["E"] == len(s)//4:
             return 0
         
-        temp, ans, left, i = defaultdict(int), len(s), 0, 0
+        temp, ans, left = defaultdict(int), len(s), 0
 
-        # while i < len(s) and (temp["Q"] < table["Q"] or temp["W"] < table["W"] or temp["E"] < table["E"] or temp["R"] < table["R"]):
-        #     temp[ s[i] ] += 1
-        #     i += 1
-        
-        # ans = i
-        # temp[s[i]] -= 1
-        # i -= 1
-
-        ans = len(s)
         for i in range( len(s) ):
             
-            # while i < len(s) and (temp["Q"] < table["Q"] or temp["W"] < table["W"] or temp["E"] < table["E"] or temp["R"] < table["R"]):
-            #     temp[ s[i] ] += 1
-            #     i += 1
             temp[ s[i] ] += 1
 
-           # remove the left part
-            while left < len(s) and temp[ s[left] ] > table[ s[left] ]:
+           # remove the left part the curr letter has more than extra
+            while left < len(s) and temp[ s[left] ] > extra[ s[left] ]:
                 temp[ s[left] ] -= 1
                 left += 1
 
-            if temp["Q"] >= table["Q"] and temp["W"] >= table["W"] and temp["E"] >= table["E"] and temp["R"] >= table["R"]:
+            # to be replaced, it should have at least the size of extra
+            # then update answer to get the minimum windows size
+            if temp["Q"] >= extra["Q"] and temp["W"] >= extra["W"] and \
+                temp["E"] >= extra["E"] and temp["R"] >= extra["R"]:
                 ans = min(ans, i - left + 1)
         
         return ans
