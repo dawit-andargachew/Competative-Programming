a768f65f0e025869ec49d454fff41ccbb1119785
Dawit Andargachew <dawit.andargachew.asmare@gmail.com>
2024-03-30 07:19:14 +0300

    Added solution for Take K of Each Character From Left and Right

--- Competative-Programming/take-k-of-each-character-from-left-and-right.py
+++ Competative-Programming/take-k-of-each-character-from-left-and-right.py
@@ -1,38 +1,34 @@
 class Solution:
     def takeCharacters(self, s: str, k: int) -> int:
         if k == 0:
-            return 0
-
-        check = Counter(s)
-        if check["a"] < k or check["b"] < k or check["c"] < k:
+            return 0 # no need to remove any elements since k is zero
+        temp = Counter(s)
+        if temp["a"] < k or temp["b"] < k or temp["c"] < k: # handle exception, we can't have k elements for each
             return -1
         
-        # instead of always start from end, start from right the to the end
-        # then when add one element from the start, remove one from the end
-        # it saves time
 
-        # remove the last elements only
+        # Start by inserting only the last elements and then progressively 
+        # add one item from the start, then remove items until k items for `a`,`b`,`c` are present in the map
+
+        # step-1: start by adding only the last elements till k items for each
         table = defaultdict(int)
         right = len(s) - 1
         while table["a"] < k or table["b"] < k or table["c"] < k:
             table[ s[right] ] += 1
             right -= 1
         right += 1
-        ans =  len(s) - right # initilized by removing the last elements only
+
+        ans =  len(s) - right # initilized the size by current windows size
 
         for i in range( len(s) ):
-            table[ s[i] ] += 1
+            table[ s[i] ] += 1 # progressively add elements from the start
             
-            # remove right until it doesn't have effect, how?
+            # remove remove from the right end, until we have k elements for each
             while right < len(s) and table[ s[right] ] - 1 >= k:
                 table[ s[right] ] -= 1
                 right += 1
 
-            # print()
-            # print("i ", i,"right ", right)
-            # print(table)
-
             windows_size = i + 1 + len(s) - right
-            ans = min(ans, windows_size)
+            ans = min(ans, windows_size) # update our answer
 
         return ans
