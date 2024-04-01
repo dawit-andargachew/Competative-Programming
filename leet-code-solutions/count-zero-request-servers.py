ab3f43a04c98f9dc0b2a07b17f2a517ac46cfccd
Dawit Andargachew <dawit.andargachew.asmare@gmail.com>
2024-04-01 22:53:31 +0300

    Added solution for Count Zero Request Servers

--- leet-code-solutions/count-zero-request-servers.py
+++ leet-code-solutions/count-zero-request-servers.py
@@ -2,26 +2,24 @@ class Solution:
     def countServers(self, n: int, logs: List[List[int]], x: int, queries: List[int]) -> List[int]:
 
         logs.sort(key=lambda x: x[1])
-        temp = queries[:]
+        temp = queries[:] # helps to keep the order of quries
         queries.sort()
 
         element_map, store = Counter(queries), defaultdict(int)
         left = right = 0
 
-        # initialize
-        while left < len(logs) and  logs[left][1] < (queries[0] - x):
-            left += 1
-        
-        right = left 
-        while right < len(logs) and logs[right][1] <= queries[0]:
-            store[ logs[right][0] ] += 1
-            right += 1
-        element_map[queries[0]] = n - len(store)
-
         # loop each query and determine non-requested servers
-        for q in range(1, len(queries)):
+        for q in range(len(queries)):
 
             if right < len(logs) and logs[right][1] < queries[q] - x:
+                # previos quries value is the currenct minimum range
+                # take this 
+                '''
+                n = 4
+                logs = [[4,3],[2,16],[1,21],[3,22],[1,13],[3,10],[2,1],[1,12],[4,13],[2,18]]
+                x = 8
+                queries = [14,28,29]
+                '''
                 store.clear()
                 left = right
 
@@ -32,9 +30,9 @@ class Solution:
                 while right < len(logs) and logs[right][1] <= queries[q]:
                     store[ logs[right][0] ] += 1
                     right += 1
-                element_map[queries[q]] = n - len(store)
+                element_map[queries[q]] = n - len(store) # store number of inactive servers
             
-            else:
+            else: # keep a sliding windows, since consecutive quieries are overlapping
                 # move left 
                 while left < right and logs[left][1] < (queries[q] - x):
                     store[ logs[left][0] ] -= 1
@@ -46,7 +44,7 @@ class Solution:
                 while right < len(logs) and logs[right][1] <= queries[q]:
                     store[ logs[right][0] ] += 1
                     right += 1
-                element_map[ queries[q] ] = n - len(store)
+                element_map[ queries[q] ] = n - len(store) # store number of inactive servers
             
         answer = []
         for i in temp:
